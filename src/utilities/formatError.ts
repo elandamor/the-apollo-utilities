00f8cc4006e2e151b006600254a9c853be945808
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2020-09-17 06:15:54 +0200

    chore(types): provide missing types

--- src/utilities/formatError.ts
+++ src/utilities/formatError.ts
@@ -1,43 +1,47 @@
-import { prefersDarkMode } from './prefersDarkMode';
+import { Maybe } from "graphql/jsutils/Maybe";
+import { prefersDarkMode } from "./prefersDarkMode";
 
-export const formatError = (message: any, path?: any) => {
+export const formatError = (
+  message: string,
+  path?: Maybe<ReadonlyArray<string | number>>
+) => {
   const color = prefersDarkMode()
     ? {
-        gray: '#AAA',
-        black: '#CCC',
+        light: "#AAA",
+        dark: "#CCC",
       }
     : {
-        gray: 'gray',
-        black: '#333',
+        light: "light",
+        dark: "#333",
       };
 
   if (path) {
     const headerCss = [
-      `color: ${color.gray}; font-weight: lighter`, // title
-      `color: ${color.black}; font-weight: 600`, // message
-      `color: ${color.gray}; font-weight: lighter`, // on
-      `color: ${color.black}; font-weight: 600`, // path
-      `color: ${color.gray}; font-weight: lighter`, // request.
+      `color: ${color.light}; font-weight: lighter`, // title
+      `color: ${color.dark}; font-weight: 600`, // message
+      `color: ${color.light}; font-weight: lighter`, // on
+      `color: ${color.dark}; font-weight: 600`, // path
+      `color: ${color.light}; font-weight: lighter`, // request.
     ];
 
-    const parts = ['%c message'];
+    const parts = ["%c message"];
 
     parts.push(`%c${message}`);
-    parts.push('%con');
-    parts.push(`%c${path.join('.')}`);
-    parts.push('%crequest');
+    parts.push("%con");
+    parts.push(`%c${path.join(".")}`);
+    parts.push("%crequest");
 
-    return [parts.join(' '), ...headerCss];
+    return [parts.join(" "), ...headerCss];
   }
 
   const headerCss = [
-    `color: ${color.gray}; font-weight: lighter`, // title
-    `color: ${color.black}; font-weight: 600`, // message
+    `color: ${color.light}; font-weight: lighter`, // title
+    `color: ${color.dark}; font-weight: 600`, // message
   ];
 
-  const parts = ['%c message'];
+  const parts = ["%c message"];
 
   parts.push(`%c${message}`);
 
-  return [parts.join(' '), ...headerCss];
+  return [parts.join(" "), ...headerCss];
 };
--- src/utilities/loggerLink.ts
+++ src/utilities/loggerLink.ts
@@ -1,29 +1,24 @@
-import { ApolloLink, NextLink, Operation } from "apollo-link";
+import { ApolloLink, NextLink, Operation } from "@apollo/client";
 import { formatMessage } from "./formatMessage";
 
 export const loggerLink = new ApolloLink(
-  (operation: Operation, forward?: NextLink): any => {
+  (operation: Operation, forward: NextLink) => {
     const startTime = new Date().getTime();
 
-    return (
-      forward &&
-      forward(operation).map((result) => {
-        // @ts-ignore - TS infers FragmentDefinitionNode instead of OperationDefinitionNode
-        const operationType = operation.query.definitions[0].operation;
-        const elapsed = new Date().getTime() - startTime;
+    return forward(operation).map((result) => {
+      // @ts-ignore - Property 'operation' does not exist on type 'FragmentDefinitionNode'
+      const operationType = operation.query.definitions[0].operation;
+      const elapsed = new Date().getTime() - startTime;
 
-        const group = formatMessage(operationType, operation, elapsed);
-        console.groupCollapsed(...group);
+      const group = formatMessage(operationType, operation, elapsed);
+      console.groupCollapsed(...group);
 
-        console.log("INIT", operation);
-        console.log("RESULT", result);
+      console.log("INIT", operation);
+      console.log("RESULT", result);
 
-        // TODO: Check if removing argument breaks intended functionality.
-        // @ts-ignore - Expects 0 arguments.
-        console.groupEnd(...group);
+      console.groupEnd();
 
-        return result;
-      })
-    );
+      return result;
+    });
   }
 );
